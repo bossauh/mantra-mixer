18198448d85ccfb334490a577158a264b0f04fa0
matt <philmattdev@gmail.com>
2022-01-26 00:40:17 +0800

    refactor:version update

--- setup.py
+++ setup.py
@@ -11,13 +11,13 @@ ffmpy==0.3.0
 setup(
     name="mantra-mixer",
     packages=["mantra_mixer"],
-    version="0.6",
+    version="0.7",
     license="MIT",
     description="Audio mixing library simplified.",
     author="Philippe Mathew",
     author_email="philmattdev@gmail.com",
     url="https://github.com/bossauh/mantra-mixer",
-    download_url="https://github.com/bossauh/mantra-mixer/archive/refs/tags/v_06.tar.gz",
+    download_url="https://github.com/bossauh/mantra-mixer/archive/refs/tags/v_07.tar.gz",
     keywords=["audio", "mixing", "mixer"],
     install_requires=install_requires,
     classifiers=[
